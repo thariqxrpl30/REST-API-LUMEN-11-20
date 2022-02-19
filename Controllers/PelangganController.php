ad7035cd78b0399b8d2a90e99b10ec439681b03b
ThariqXIRPL29 <87298575+thariqxrpl30@users.noreply.github.com>
2022-02-19 14:58:08 +0700

    Add files via upload

---
+++ Controllers/PelangganController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelanggan;
+use Illuminate\Http\Request;
+
+class PelangganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Pelanggan::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //membuat validation
+        $this->validate($request, [
+            'pelanggan' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required | numeric'
+        ]);
+
+        $pelanggan = Pelanggan::create($request->all());
+
+        return response()->json($pelanggan);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //menampilkan satu data saja
+        $data = Pelanggan::where('idpelanggan', $id)->get();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelanggan $pelanggan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Pelanggan::where('idpelanggan',$id)->update($request->all());
+
+        return response()->json("data berhasil di update");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Pelanggan::where('idpelanggan',$id)->delete(); //setelah idnya ketemu maka lgsg mjlnkn funtion delete
+
+        return response()->json("Data sudah dihapus");
+    }
+}
